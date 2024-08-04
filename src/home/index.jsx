96bae5de20a75cdeb8a24329b8c6ce0a0e203357
Rajesh Dukiya <112774887+Rajeshdukiya@users.noreply.github.com>
2024-08-04 17:59:39 +0530

    header-section

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -1,10 +1,14 @@
-import { UserButton } from '@clerk/clerk-react';
-import React from 'react'
+import Header from "@/components/custom/Header";
+import { UserButton } from "@clerk/clerk-react";
+import React from "react";
 
 const Home = () => {
   return (
-    <div><UserButton/></div>
-  )
-}
+    <div>
+      <Header/>
+      Landing Screen
+    </div>
+  );
+};
 
 export default Home;
