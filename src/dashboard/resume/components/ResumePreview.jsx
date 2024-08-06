78aa311e5175a236fc094bed42439cf572671a78
Rajesh Dukiya <112774887+Rajeshdukiya@users.noreply.github.com>
2024-08-06 22:12:59 +0530

    resume preview-section

---
+++ src/dashboard/resume/components/ResumePreview.jsx
@@ -0,0 +1,32 @@
+import { ResumeInfoContext } from '@/context/ResumeInfoContext'
+import React, { useContext } from 'react'
+import PersonalDetailPreview from './preview/PersonalDetailPreview'
+import SummeryPreview from './preview/SummeryPreview'
+import ExperiencePreview from './preview/ExperiencePreview'
+import EducationalPreview from './preview/EducationalPreview'
+import SkillsPreview from './preview/SkillsPreview'
+
+const ResumePreview = () => {
+
+  const {resumeInfo, setResumeInfo} = useContext(ResumeInfoContext)
+
+  return (
+    <div className='shadow-lg h-full p-14 border-t-[20px]'
+     style={{
+      borderColor:resumeInfo?.themeColor
+     }}>
+      {/* Personal Detail */}
+          <PersonalDetailPreview resumeInfo = {resumeInfo}/> 
+      {/* Sumery */}
+          <SummeryPreview resumeInfo = {resumeInfo}/>
+      {/* Professional Experience */}
+          <ExperiencePreview resumeInfo = {resumeInfo}/>
+      {/* Educational */}
+          <EducationalPreview resumeInfo = {resumeInfo}/>
+      {/* Skills */}
+          <SkillsPreview resumeInfo = {resumeInfo}/>
+    </div>
+  )
+}
+
+export default ResumePreview
