55797050e9296796414631b087a84720ac08f935
Rajesh Dukiya <112774887+Rajeshdukiya@users.noreply.github.com>
2024-08-07 17:56:03 +0530

    experience-section

--- src/dashboard/resume/components/FormSection.jsx
+++ src/dashboard/resume/components/FormSection.jsx
@@ -1,24 +1,21 @@
-import React, { useState } from 'react'
-import PersonalDetail from './forms/PersonalDetail'
-import { Button } from '@/components/ui/button'
-import { ArrowLeft, ArrowRight, LayoutGrid } from 'lucide-react'
-import Summery from './forms/Summery';
-
-
+import React, { useState } from "react";
+import PersonalDetail from "./forms/PersonalDetail";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft, ArrowRight, LayoutGrid } from "lucide-react";
+import Summery from "./forms/Summery";
+import Experience from "./forms/Experience";
 
 const FormSection = () => {
-  const [activeFormIndex,setActiveFormIndex]=useState(1);
-  const [enableNext,setEnableNext]=useState(true);
- 
+  const [activeFormIndex, setActiveFormIndex] = useState(1);
+  const [enableNext, setEnableNext] = useState(true);
+
   return (
     <div>
       <div className="flex justify-between items-center">
         <div className="flex gap-5">
-       
-          <Button variant="outline" size="sm" className="flex gap-2"><LayoutGrid/></Button>
-          
-         
-         
+          <Button variant="outline" size="sm" className="flex gap-2">
+            <LayoutGrid />
+          </Button>
         </div>
         <div className="flex gap-2">
           {activeFormIndex > 1 && (
@@ -43,11 +40,13 @@ const FormSection = () => {
         </div>
       </div>
       {/* Personal Detail */}
-      {
-        activeFormIndex==1? 
-      <PersonalDetail enabledNext={(v)=>setEnableNext(v)} />
-      :activeFormIndex==2?<Summery  enabledNext={(v)=>setEnableNext(v)} />
-      :null}
+      {activeFormIndex == 1 ? (
+        <PersonalDetail enabledNext={(v) => setEnableNext(v)} />
+      ) : activeFormIndex == 2 ? (
+        <Summery enabledNext={(v) => setEnableNext(v)} />
+      ) : activeFormIndex == 3 ? (
+        <Experience/>
+      ) : null}
       {/* Summery */}
 
       {/* Experience */}
--- src/dashboard/resume/components/forms/Experience.jsx
+++ src/dashboard/resume/components/forms/Experience.jsx
@@ -1,9 +1,151 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import RichTextEditor from "../RichTextEditor";
+import { ResumeInfoContext } from "@/context/ResumeInfoContext";
+
+const formField = {
+  title: "",
+  companyName: "",
+  city: "",
+  state: "",
+  startDate: "",
+  endDate: "",
+  workSummery: "",
+};
 
 const Experience = () => {
-  return (
-    <div>Experience</div>
-  )
+  const [experienceList, setExperienceList] = useState([formField]);
+  const {resumeInfo,setResumeInfo} = useContext(ResumeInfoContext);
+
+  const [loading,setLoading]=useState(false);
+
+  const handleChange = (index, event) => {
+    const newEntries=experienceList.slice();
+    const {name ,value}= event.target;
+    newEntries[index][name]=value;
+    setExperienceList(newEntries);
+  };
+
+  const AddNewExperience=()=>{
+    setExperienceList([...experienceList,formField])
+  }
+
+  const RemoveExperience=()=>{
+    setExperienceList(experienceList=>experienceList.slice(0,-1))
+  }
+
+  const handleRichTextEditor = (e,name,index)=>{
+    const newEntries=experienceList.slice();
+    newEntries[index][name]=e.target.value;
+    setExperienceList(newEntries)
+  }
+
+  const onSave=()=>{
+    setLoading(true)
+    const data={
+        data:{
+            Experience:experinceList.map(({ id, ...rest }) => rest)
+        }
+    }
+
+     console.log(experienceList)
+
+    GlobalApi.UpdateResumeDetail(params?.resumeId,data).then(res=>{
+        console.log(res);
+        setLoading(false);
+        toast('Details updated !')
+    },(error)=>{
+        setLoading(false);
+    })
+
 }
 
-export default Experience
+  useEffect(()=>{
+    setResumeInfo({
+      ...resumeInfo,
+      Experience:experienceList
+  });
+  },[experienceList])
+
+  return (
+    <div>
+      <div className="p-5 shadow-lg rounded-lg border-t-primary border-t-4 mt-10">
+        <h2 className="font-bold text-lg">Professional Experience</h2>
+        <p>Add Your previous Job experience</p>
+        <div>
+          {experienceList.map((item, index) => (
+            <div key={index}>
+              <div className="grid grid-cols-2 gap-3 border p-3 my-5 rounded-lg">
+                <div>
+                  <label className="text-xs">Position Title</label>
+                  <Input
+                    name="title"
+                    onChange={(event) => handleChange(index, event)}
+                  />
+                </div>
+                <div>
+                  <label className="text-xs">company Name</label>
+                  <Input
+                    name="companyname"
+                    onChange={(event) => handleChange(index, event)}
+                  />
+                </div>
+                <div>
+                  <label className="text-xs">City</label>
+                  <Input
+                    name="city"
+                    onChange={(event) => handleChange(index, event)}
+                  />
+                </div>
+                <div>
+                  <label className="text-xs">State</label>
+                  <Input
+                    name="state"
+                    onChange={(event) => handleChange(index, event)}
+                  />
+                </div>
+                <div>
+                  <label className="text-xs">Start Date</label>
+                  <Input
+                    type="date"
+                    name="startdate"
+                    onChange={(event) => handleChange(index, event)}
+                  />
+                </div>
+                <div>
+                  <label className="text-xs">End Date</label>
+                  <Input
+                    type="date"
+                    name="enddate"
+                    onChange={(event) => handleChange(index, event)}
+                  />
+                </div>
+                <div className='col-span-2'>
+                           {/* Work Summery  */}
+                           <RichTextEditor
+                           index={index}
+                           defaultValue={item?.workSummery}
+                           onRichTextEditorChange={(event)=>handleRichTextEditor(event,'workSummery',index)}  />
+                        </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className='flex justify-between'>
+            <div className='flex gap-2'>
+            <Button variant="outline" onclick={AddNewExperience}  className="text-primary"> + Add More Experience</Button>
+            <Button variant="outline" onclick={RemoveExperience} className="text-primary"> - Remove</Button>
+
+            </div>
+             <Button disabled={loading} onClick={()=>onSave()}>
+            {loading?<LoaderCircle className='animate-spin' />:'Save'}    
+            </Button>
+            
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
